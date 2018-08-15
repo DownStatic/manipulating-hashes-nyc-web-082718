2eeda4c27437e667951151b804e00192e8a7ab41
Christopher Stull <chris.stull@gmail.com>
2018-08-15 22:00:52 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,9 @@ def first_challenge
   #your code here
 contact["Freddy Mercury"].each do |key,value|
   if key == :favorite_icecream_flavors
-    value.delete_if | == "strawberry"
-        
+    value.delete_if {|flavor| flavor == "strawberry"}
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
